8dc39bb4d75ad6c9d44d8288bf46de67cacc3089
reverse <129334336+revrsedev@users.noreply.github.com>
2024-07-31 02:49:39 +0200

    Update m_changeidentonick.cpp

--- m_changeidentonick.cpp
+++ m_changeidentonick.cpp
@@ -1,3 +1,25 @@
+/*
+ * InspIRCd -- Internet Relay Chat Daemon
+ *
+ *   Copyright (C) 2024 Jean Chevronnet <mike.chevronnet@gmail.com>
+ *
+ * This file contains a third party module for InspIRCd.  You can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/// $ModAuthor: Jean Chevronnet (reverse) <mike.chevronnet@gmail.com>
+/// $ModDesc: Sets the user's ident to match their nickname on connect.
+/// $ModDepends: core 4
+
 #include "inspircd.h"
 
 class CommandSetNickIdent : public Command
